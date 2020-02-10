e0fde6327fc774e91fa93555c78062b46a191660
Jakob Haglöf <j.haglof56@gmail.com>
2020-02-10 11:18:34 +0100

    working lib for connecting to sinch api

--- sinch/sinch.go
+++ sinch/sinch.go
@@ -2,20 +2,45 @@ package sinch
 
 import (
 	"fmt"
-	log "github.com/sirupsen/logrus"
 	"io"
 	"net/http"
+
+	log "github.com/sirupsen/logrus"
 )
 
 // Send Handles Post requests to the sinch api... returns nil if successful
-func Send(url string, body io.Reader) error {
-	resp, err := http.Post(url, "Content-Type: application/json", body)
+func Send(url string, token string, body io.Reader) (*http.Response, error) {
+
+	//jsonData, err := json.Marshal(body)
+
+	req, _ := http.NewRequest("POST", url, body)
+
+	req.Header.Add("Content-Type", "application/json")
+	req.Header.Add("Authorization", fmt.Sprintf("Bearer %s", token))
+
+	client := http.Client{}
+
+	resp, err := client.Do(req)
 
 	if err != nil {
-		return fmt.Errorf("Post call failed: %w", err)
+		return nil, fmt.Errorf("Post call failed: %w", err)
 	}
 
 	log.Infof("Response body: %v", resp.Body)
 
-	return nil
+	return resp, nil
 }
+
+/*
+POST \
+     -H "Authorization: Bearer {token}" \
+     -H "Content-Type: application/json"  -d '
+      {
+          "from": "12345",
+          "to": [
+              "123456789"
+          ],
+          "body": "Hi there! How are you?"
+      }' \
+  "https://eu.sms.api.sinch.com/xms/v1/{service_plan_id}/batches"
+*/
